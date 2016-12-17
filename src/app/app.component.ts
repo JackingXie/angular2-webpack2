7fb6070cbd96bc4de905c67bf82346410f6c1c3c
Jacking Xie <clisely@gmail.com>
2016-12-17 12:00:30 +0800

    Create the base framework with Angular 2 and Webpack 2.
    Based on the angular2-webpack by preboot.
    https://github.com/preboot/angular2-webpack

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component }  from '@angular/core';
+
+import { ApiService } from './shared';
+
+import '../style/app.scss';
+
+@Component({
+  	selector: 'my-app',
+  	templateUrl: './app.component.html',
+  	styleUrls: [ './app.component.scss' ],
+})
+export class AppComponent {
+  	url: string = 'https://github.com/preboot/angular2-webpack';
+
+  	constructor(private api: ApiService) {
+		// Do something with api
+  	}
+}
